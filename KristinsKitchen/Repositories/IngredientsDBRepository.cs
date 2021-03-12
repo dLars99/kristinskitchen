29c9a7d00afcbbe92134e39dc71304bf3bf49ee0
David Larsen <davidplarsen@gmail.com>
2021-03-11 22:37:39 -0600

    Outline GetAllIngredients controller

---
+++ KristinsKitchen/Repositories/IngredientsDBRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using KristinsKitchen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KristinsKitchen.Repositories
+{
+    public class IngredientsDBRepository : BaseRespository
+    {
+        public IngredientsDBRepository(IConfiguration configuration) : base(configuration) { }
+        
+        public List<IngredientsDB> GetAllIngredients()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "@";
+                    var reader = cmd.ExecuteReader();
+                    var ingredientList = new List<IngredientsDB>();
+                    reader.Close();
+                    return ingredientList;
+                }
+            }
+        }
+    }
+}
