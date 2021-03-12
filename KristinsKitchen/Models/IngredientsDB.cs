b44c65f6962e6836f425ef3f2115048272f5a534
David Larsen <davidplarsen@gmail.com>
2021-03-11 22:04:48 -0600

    Build models for IngredientsDB

---
+++ KristinsKitchen/Models/IngredientsDB.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KristinsKitchen.Models
+{
+    public class IngredientsDB
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Description { get; set; }
+        [MaxLength(255)]
+        public string Variety { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        public decimal Quantity { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string QuantityUnit { get; set; }
+        [MaxLength(255)]
+        public string ContainerType { get; set; }
+        [Required]
+        public int PantryShelfLife { get; set; }
+        [Required]
+        public int FridgeShelfLife { get; set; }
+        [Required]
+        public int FreezerShelfLife { get; set; }
+        [MaxLength(255)]
+        public string ImageLocation { get; set; }
+    }
+}
---
+++ KristinsKitchen/Models/Location.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KristinsKitchen.Models
+{
+    public class Location
+    {
+        public int id { get; set; }
+        [Required]
+        [MaxLength(12)]
+        public string LocationName { get; set; }
+    }
+}
