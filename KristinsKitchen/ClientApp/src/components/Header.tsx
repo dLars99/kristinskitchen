91c87e99309da4e25c824cfcf52e82f83799328a
David Larsen <davidplarsen@gmail.com>
2021-06-08 11:34:18 -0500

    Start Material UI header bar

--- KristinsKitchen/ClientApp/src/components/Header.tsx
+++ KristinsKitchen/ClientApp/src/components/Header.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import AccountCircle from '@material-ui/icons/AccountCircle'
 
 const Header = () => {
 
+    const handleMenu = (): void => {
+        console.log("Hi")
+    }
+
     return (
-        <h1>Hello World</h1>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography>Kristin's Kitchen</Typography>
+            </Toolbar>
+            <div>
+                <IconButton
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                    color="inherit"
+                >
+                    <AccountCircle />
+                </IconButton>
+            </div>
+        </AppBar>
     )
 }
 
