1653af22510da98fbd2f15e05933e7db905bbbf3
David Larsen <davidplarsen@gmail.com>
2021-06-29 20:46:15 -0500

    Adjust header layout

--- KristinsKitchen/ClientApp/src/components/Header.tsx
+++ KristinsKitchen/ClientApp/src/components/Header.tsx
@@ -1,33 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import AccountCircle from '@material-ui/icons/AccountCircle'
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
 const Header = () => {
 
-    const handleMenu = (): void => {
-        console.log("Hi")
+    const classes = useStyles();
+    const [menuAnchorEl, setMenuAnchorEl] = useState<HTMLButtonElement>();
+
+    const handleMenu = (event: React.MouseEvent<HTMLButtonElement>): void => {
+        setMenuAnchorEl(event.currentTarget);
     }
 
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <Typography>Kristin's Kitchen</Typography>
-            </Toolbar>
-            <div>
-                <IconButton
-                    aria-label="account of current user"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleMenu}
-                    color="inherit"
-                >
-                    <AccountCircle />
-                </IconButton>
-            </div>
-        </AppBar>
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>Kristin's Kitchen</Typography>
+                    <div>
+                        <IconButton
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleMenu}
+                            color="inherit"
+                        >
+                            <AccountCircle />
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
     )
 }
 
